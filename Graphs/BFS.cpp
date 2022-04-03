2e0a094a1fbfd731d1625703d0fea918d5365365
Andrea Alegre <ovyfe4@PL1FRTLS0295NB.net.plm.eds.com>
2022-04-03 13:16:09 +0200

    Graphs 1: Breadth First Search

---
+++ Graphs/BFS.cpp
@@ -0,0 +1,168 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'bfs' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER m
+ *  3. 2D_INTEGER_ARRAY edges
+ *  4. INTEGER s
+ */
+
+
+vector<int> bfs(int n, int m, vector<vector<int>> edges, int s)
+{
+    // 1. BUILD GRAPH WITH DATA
+    // Create a map with a list of all contiguous nodes for a given node
+    unordered_map<int, vector<int>> nodeToNeighbors;
+    cout << "s: "<< s << endl;
+    for(auto edge: edges)
+    {
+        nodeToNeighbors[edge[0]].push_back(edge[1]);
+        nodeToNeighbors[edge[1]].push_back(edge[0]);
+        cout << edge[0]<<"; "<< edge[1] << endl;
+    }
+
+    // 2.Start BFS
+    vector<int> distances;
+    for(int i = 0; i < n; i++)
+    {
+        distances.push_back(-1);
+    }
+    queue<int> nextToVisit;
+    nextToVisit.push(s);
+    
+    unordered_set<int> visitedNodes;
+    int sIdx = s-1;
+    distances[sIdx] = 0;
+    visitedNodes.insert(s);
+
+    while(!nextToVisit.empty())
+    {
+        // Get first node in queue to be visited
+        int node = nextToVisit.front();
+        nextToVisit.pop();
+        int idx = node - 1;
+        
+        // For each of the node's neighbors
+        for(auto neighbor: nodeToNeighbors[node])
+        {
+            int adjIdx = neighbor - 1;
+            if(visitedNodes.find(neighbor)==visitedNodes.end())
+            {
+                nextToVisit.push(neighbor);// add unvisited neighbor
+                visitedNodes.insert(neighbor); // mark neighbor as visited
+                // increase distance
+                distances[adjIdx] = distances[idx]+6;
+            }
+        }
+    }
+    distances.erase(distances.begin() + sIdx);
+    return distances;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string q_temp;
+    getline(cin, q_temp);
+
+    int q = stoi(ltrim(rtrim(q_temp)));
+
+    for (int q_itr = 0; q_itr < q; q_itr++) {
+        string first_multiple_input_temp;
+        getline(cin, first_multiple_input_temp);
+
+        vector<string> first_multiple_input = split(rtrim(first_multiple_input_temp));
+
+        int n = stoi(first_multiple_input[0]);
+
+        int m = stoi(first_multiple_input[1]);
+
+        vector<vector<int>> edges(m);
+
+        for (int i = 0; i < m; i++) {
+            edges[i].resize(2);
+
+            string edges_row_temp_temp;
+            getline(cin, edges_row_temp_temp);
+
+            vector<string> edges_row_temp = split(rtrim(edges_row_temp_temp));
+
+            for (int j = 0; j < 2; j++) {
+                int edges_row_item = stoi(edges_row_temp[j]);
+
+                edges[i][j] = edges_row_item;
+            }
+        }
+
+        string s_temp;
+        getline(cin, s_temp);
+
+        int s = stoi(ltrim(rtrim(s_temp)));
+
+        vector<int> result = bfs(n, m, edges, s);
+
+        for (size_t i = 0; i < result.size(); i++) {
+            fout << result[i];
+
+            if (i != result.size() - 1) {
+                fout << " ";
+            }
+        }
+
+        fout << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
